889ef0508269f5cc6f14183474b7f640c20ebadf
armin shv <armin83shahverdi@gmail.com>
2024-08-26 12:03:11 +0330

    use templates

--- Home/urls.py
+++ Home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.say_hello),
-    path('', views.home_page),
+    path('', views.home),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def say_hello(request):
-    return HttpResponse("Hello user...")
+    return render(request, "hello.html")
 
-def home_page(request):
-    return HttpResponse("welcome to my site")
+def home(request):
+    return render(request, "home.html")
+
